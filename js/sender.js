9c5eb39621fc5c63aa958f08a63f641bdc1129b0
rungtruong <rung.iuh@gmail.com>
2023-02-01 15:24:15 +0700

    Update sender.js

--- js/sender.js
+++ js/sender.js
@@ -1,37 +1,34 @@
-(function ($) {
-  var form = document.getElementById("contact-form");
+var form = document.getElementById("contact-form");
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    var status = document.getElementById("contact-form-status");
-    var data = new FormData(event.target);
-    fetch(event.target.action, {
-      method: form.method,
-      body: data,
-      headers: {
-        Accept: "application/json",
-      },
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("contact-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+          }
+        });
+      }
     })
-      .then((response) => {
-        if (response.ok) {
-          status.innerHTML = "Thanks for your submission!";
-          form.reset();
-        } else {
-          response.json().then((data) => {
-            if (Object.hasOwn(data, "errors")) {
-              status.innerHTML = data["errors"]
-                .map((error) => error["message"])
-                .join(", ");
-            } else {
-              status.innerHTML =
-                "Oops! There was a problem submitting your form";
-            }
-          });
-        }
-      })
-      .catch((error) => {
-        status.innerHTML = "Oops! There was a problem submitting your form";
-      });
-  }
-  form.addEventListener("submit", handleSubmit);
-});
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+form.addEventListener("submit", handleSubmit);

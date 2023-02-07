2417550ef41a479bab30fa83ab12588d3bb56de6
rungtruong <rung.iuh@gmail.com>
2023-02-07 11:33:09 +0700

    Update sender.js

--- js/sender.js
+++ js/sender.js
@@ -1,34 +1,35 @@
 var form = document.getElementById("contact-form");
+var formStatus = document.getElementById("contact-form-formStatus");
 
 async function handleSubmit(event) {
   event.preventDefault();
-  var status = document.getElementById("contact-form-status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset();
-      } else {
-        response.json().then((data) => {
-          if (Object.hasOwn(data, "errors")) {
-            status.innerHTML = data["errors"]
-              .map((error) => error["message"])
-              .join(", ");
-          } else {
-            status.innerHTML = "Oops! There was a problem submitting your form";
-          }
-        });
-      }
-    })
-    .catch((error) => {
-      status.innerHTML = "Oops! There was a problem submitting your form";
+  const data = new FormData(event.target);
+
+  try {
+    const response = await fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        Accept: "application/json",
+      },
     });
+
+    if (!response.ok) {
+      throw new Error("Fetch failed");
+    }
+
+    const json = await response.json();
+    if (json.hasOwnProperty("errors")) {
+      formStatus.innerHTML = json.errors
+        .map((error) => error.message)
+        .join(", ");
+    } else {
+      formStatus.innerHTML = "Thanks for your submission!";
+      form.reset();
+    }
+  } catch (error) {
+    formStatus.innerHTML = "Oops! There was a problem submitting your form";
+  }
 }
+
 form.addEventListener("submit", handleSubmit);

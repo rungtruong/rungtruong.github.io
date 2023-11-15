2a117627546f23f174291dadd4f77f576fe4925f
foresttruong <rung.truong@vieon.vn>
2023-11-15 17:22:33 +0700

    add story vie

---
+++ src/vie-web-ui/assets/introduction.stories-c700c934.js
@@ -0,0 +1,29 @@
+import{M as i}from"./chunk-HLWAVYOI-687d77c0.js";import{j as e}from"./jsx-runtime-ffb262ed.js";import{u as r}from"./index-a1cf9e47.js";import"./iframe-7aefb5b6.js";import"../sb-preview/runtime.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-95003468.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./index-356e4a49.js";function s(t){const n=Object.assign({p:"p",img:"img",h1:"h1",h2:"h2",h3:"h3",pre:"pre",code:"code"},r(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Introduction"}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"https://upload.wikimedia.org/wikipedia/commons/c/c6/Logo_VieON_2021.png",alt:"Logo"})}),`
+`,e.jsx(n.h1,{id:"welcome-to-vieon-design-system",children:"Welcome to VieON Design System"}),`
+`,e.jsx(n.h2,{id:"getting-started",children:"Getting started"}),`
+`,e.jsx(n.h3,{id:"how-to-use-vie-web-ui",children:"How to use vie-web-ui"}),`
+`,e.jsx(n.p,{children:"Install via npm:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-sh",children:`npm install vie-web-ui
+`})}),`
+`,e.jsx(n.p,{children:"Install via yarn:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-sh",children:`yarn add vie-web-ui
+`})}),`
+`,e.jsx(n.p,{children:"Then you need to import main.css to jsx or tsx main file on your project:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import 'vie-web-ui/main.css';
+`})}),`
+`,e.jsx(n.p,{children:"Then you are ready to import components and use them on your project:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { Button } from 'vie-web-ui';
+
+export default function Home() {
+  return (
+    <main className=" flex min-h-screen flex-col items-center justify-center">
+        <Button size="lg" variant="default">
+          Hello World 🌍!
+        </Button>
+    </main>
+  );
+}
+
+`})})]})}function a(t={}){const{wrapper:n}=Object.assign({},r(),t.components);return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}const c=()=>{throw new Error("Docs-only story")};c.parameters={docsOnly:!0};const o={title:"Introduction",tags:["stories-mdx"],includeStories:["__page"]};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:a};const y=["__page"];export{y as __namedExportsOrder,c as __page,o as default};
+//# sourceMappingURL=introduction.stories-c700c934.js.map

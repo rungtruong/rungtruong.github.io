8c7ef3ffaf6bc067458f782e097731c55fe0a6e1
rungtruong <41542332+rungtruong@users.noreply.github.com>
2023-12-20 01:17:45 +0700

    UPDATE

--- vie-web-ui/assets/preview-63d79bb5.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-3575303d.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-03d6ea92.js"),["./DocsRenderer-NNNQARDV-03d6ea92.js","./chunk-HLWAVYOI-b62d515e.js","./iframe-3575303d.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./index-95003468.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-63d79bb5.js.map
